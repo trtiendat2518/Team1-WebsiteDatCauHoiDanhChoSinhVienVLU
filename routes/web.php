3b2441855ab964510fc73365739e2089a58806d6
vominhhung <vominhhung5102000@gmail.com>
2021-05-18 19:50:26 +0700

    Tao giao dien trang chu

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 
+//STUDENT HOME
+Route::get('/', 'HomeController@index');
+
+
+
 Route::get('/login', 'StudentController@index_login');
 Route::get('/register', 'StudentController@index_register');
 Route::get('/forgetpass', 'StudentController@index_forgetpass');
