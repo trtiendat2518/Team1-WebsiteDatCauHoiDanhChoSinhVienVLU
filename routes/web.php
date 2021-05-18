d483bb61daeb224df6a000e2ce1c4da08ae72a16
trantiendat <hoangvu.tr2518@gmail.com>
2021-05-18 19:39:24 +0700

    Hoan thanh chuc nang xac thuc Mail

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/forgetpass', 'StudentController@index_forgetpass');
 Route::get('/newpass', 'StudentController@index_newpass');
+
+//STUDENT MAIL
+Route::get('/xac-nhan-tai-khoan-email', 'MailController@verifymail');
+Route::get('/xac-nhan-mail-mat-khau-moi', 'MailController@verifychangepassword');
