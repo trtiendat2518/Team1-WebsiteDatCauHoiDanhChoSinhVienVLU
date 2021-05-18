b9c9bbb1670fa6ef214053b67277fd7bd18b5ab7
vominhhung <vominhhung5102000@gmail.com>
2021-05-18 19:32:10 +0700

    Hoan thanh chuc nang Quen Mat Khau

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -16,7 +16,7 @@ session_start();
 
 class StudentController extends Controller
 {
-<<<<<<< HEAD
+
 	public function AuthLogin(){
 		$student_id=Session::get('student_id');
 		if($student_id){
@@ -44,7 +44,7 @@ class StudentController extends Controller
 		}
 	}
 
-		public function login(Request $request){
+	public function login(Request $request){
 		$data = $request->validate([
 			'student_email'=>'required|email',
 			'student_password'=>'required|min:6|max:32',
@@ -144,7 +144,8 @@ class StudentController extends Controller
 			Session::put('message', '<div class="alert alert-danger">Vui lòng nhập Mail Văn Lang!</div>');
 			return Redirect::to('/register');
 		}
-=======
+	}
+
 	public function index_forgetpass(){
 		return view('student.account.forgetpass');
 	}
@@ -155,6 +156,54 @@ class StudentController extends Controller
 		}else{
 			return view('student.account.forgetpass');
 		}	
->>>>>>> trantiendat
+	}
+	public function confirm_mail(Request $request){
+		$data = $request->all();
+		$student_email = $data['student_email'];
+		$check_exist = Student::where('student_email', $student_email)->first();
+		if(empty($student_email)){
+			Session::put('message','<div class="alert alert-danger">Vui lòng nhập mail!</div>');
+			return Redirect::to('forgetpass');
+		}
+		if($check_exist){
+			$to_name = "BCN Khoa CNTT";
+			$to_email = $check_exist->student_email;
+			$data = array("name"=>"Vui lòng xác nhận tài khoản để tiếp tục!", "body"=>"Nhấn xác nhận để tiếp tục");
+			Mail::send('mail.send_mail_change_pass', $data, function($message) use ($to_name, $to_email){
+				$message->to($to_email)->subject('Quên mật khẩu');
+				$message->from($to_email, $to_name);
+			});
+
+			Session::put('student_email', $student_email);
+			Session::put('message','<div class="alert alert-warning">Xác nhận Mail để tiếp tục đổi mật khẩu mới!</div>');
+			return Redirect::to('forgetpass');
+		}else{
+			Session::put('message','<div class="alert alert-danger">Mail không tồn tại!</div>');
+			return Redirect::to('forgetpass');
+		}
+	}
+
+	public function newpassword(Request $request){
+		$data = $request->all();
+		$student_email = Session::get('student_email');
+		$student_password = $data['student_password'];
+		$student_password_confirm = $request->student_password_confirm;
+
+		if(empty($student_password) || empty($student_password_confirm)){
+			Session::put('message','<div class="alert alert-danger">Vui lòng điền đầy đủ thông tin!</div>');
+			return Redirect::to('newpass');
+		}else if($student_password != $student_password_confirm){
+			Session::put('message','<div class="alert alert-danger">Mật khẩu không khớp với nhau!</div>');
+			return Redirect::to('newpass');
+		}else{
+			$student = Student::where('student_email', $student_email)->first();
+			$student->student_password = md5($student_password);
+			$student->save();
+
+			Session::put('token_pass', null);
+			Session::put('student_email', null);
+			Session::put('message','<div class="alert alert-success">Đổi mật khẩu thành công! Mời bạn đăng nhập.</div>');
+			return Redirect::to('login');
+		}
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 //STUDENT ACCOUNT
 Route::get('/login', 'StudentController@index_login');
 Route::get('/register', 'StudentController@index_register');
 Route::post('/dang-nhap', 'StudentController@login');
 Route::post('/dang-ky', 'StudentController@register');
 Route::get('/dang-xuat', 'StudentController@logout');
-=======
+
 Route::get('/forgetpass', 'StudentController@index_forgetpass');
 Route::get('/newpass', 'StudentController@index_newpass');
->>>>>>> trantiendat
+
