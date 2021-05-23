830ae2131af8c2f96e23b147e1d9092d859071ef
nguyentuankiet <kietnguyen20101010@gmail.com>
2021-05-23 15:06:09 +0700

    Xem cau hoi theo loai

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+use App\Models\Category;
+use App\Models\Post;
+use DB;
+use Mail;
+use Session;
+session_start();
 
 class CategoryController extends Controller
 {
-    //
+    public function show_category_post($category_id)
+    {
+    	$category_post = Category::orderBy('category_id', 'DESC')->get();
+
+    	$category_by_id = Category::join('tbl_post','tbl_post.category_id','=','tbl_category.category_id')
+    	->where('tbl_post.category_id', $category_id)->get();
+
+    	$category_by_name = Category::where('tbl_category.category_id', $category_id)->limit(1)->get();
+
+    	return view('student.page.category.show')->with(compact('category_post', 'category_by_id', 'category_by_name'));
+    }
 }
