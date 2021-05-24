588c2742f29d69f7e3ed3b36f434771b62cef98c
maiduyluan <maiduyluan200@gmail.com>
2021-05-24 18:46:51 +0700

    Thuc hien SEO cho giao dien tim kiem cau hoi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,10 +17,24 @@ session_start();
 
 class HomeController extends Controller
 {
-	public function index(){
+	public function index(Request $request){
+		//SEO
+		$meta_desc = "Website đặt câu hỏi dành cho sinh viên Văn Lang";
+        $meta_title = "Trang chủ";
+        $url_canonical = $request->url();
+		//---------------
+		
 		$category_post = Category::orderBy('category_id', 'DESC')->get();
 		$post = Post::join('tbl_category','tbl_category.category_id','=','tbl_post.category_id')
-		->orderBy('post_id','DESC')->paginate(2);
-		return view('student.page.home')->with(compact('category_post','post'));
+		->orderBy('post_id','DESC')->paginate(10);
+		return view('student.page.home')->with(compact('meta_desc','meta_title','url_canonical','category_post','post'));
+	}
+
+	public function search(Request $request)
+	{
+		$category_post = Category::orderBy('category_id', 'DESC')->get();
+		$keywords = $request->keywords_submit;
+		$search_product = Post::join('tbl_category','tbl_category.category_id','=','tbl_post.category_id')->where('post_title','like','%'.$keywords.'%')->get(); 
+		return view('student.page.post.search')->with(compact('category_post','search_product'));
 	}
 }
