781e2e51a5a0d664b692e450472bf3acba51bc3d
nguyenduyquang <250412quang@gmail.com>
2021-06-21 05:06:07 +0700

    Hoan thanh chuc nang xem chi tiet cau hoi 100LIKE tro len

--- routes/web.php
+++ routes/web.php
@@ -113,6 +113,7 @@ Route::get('/huy-ghim-cau-hoi/{post_id}', 'PostController@postadmin_unpin');
 //ADMIN HOT POST
 Route::get('/cau-hoi-dang-chu-y', 'PostController@postadmin_listhot');
 Route::post('/tim-kiem-cau-hoi-dang-chu-y','PostController@postadmin_searchhot');
+Route::get('/xem-cau-hoi-dang-chu-y/{post_id}', 'PostController@postadmin_detailhot');
 
 //ADMIN REPLY
 Route::post('/tra-loi-cau-hoi/{post_id}', 'ReplyController@reply_post');
